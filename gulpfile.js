f075142c23127e37603e554d212399d0bb3b7c12
Mohammed Azzam <mohammed.mojahed93@gmail.com>
2020-02-11 22:47:47 +0200

    (4)عمل بنية الملفات  كتابة أول تاسك مع الفانكشن

--- gulpfile.js
+++ gulpfile.js
@@ -6,3 +6,15 @@ var imagemin = require('gulp-imagemin');
 var rename = require('gulp-rename');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+
+
+function styles(){
+    return gulp.src('src/scss/styles.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({ outputStyle: 'compressed' })).on('error', sass.logError)
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write())
+    .pipe(rename('main.min.css'))
+    .pipe(gulp.dest('dist/css'))
+}
+exports.styles = styles;
